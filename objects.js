a485393f52238fa04d71069cc74a030e653cf4ec
Matthew Hunger <matt.hunger@gmail.com>
2017-09-03 01:08:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,7 +7,7 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist['Phil Ochs']
+function removeFromPlaylist(playlist, Kanye) {
+  delete playlist.Kanye
   return playlist
 }
