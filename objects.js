ed5073343346967af2472468b5abad69d499ec20
Matthew Hunger <matt.hunger@gmail.com>
2017-09-03 01:06:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,6 @@
 var playlist = {smiths: 'rusholm ruffians'}
 
 
-['smiths'] = 'rusholm ruffians'
-
-
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
