68ceb8f26d694c60dc2aeae7d4fce242ea804f36
Matthew Hunger <matt.hunger@gmail.com>
2017-09-03 01:04:35 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,3 +6,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Phil Ochs'] = "Here's to the State of Mississippi"
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist['Phil Ochs']
+  return playlist
+}
